a2814e64e780534da1fb8f979735c990fe9fd8bf
vinicius795 <vinicius795@hotmail.com>
2020-11-05 16:19:51 +0000

    Deploying to gh-pages from  @ 72c22a9f7791df199db75104fd8300a4cd295d91 🚀

--- main.js
+++ main.js
@@ -512,31 +512,31 @@ const submenuslist = [
     { id: 1, title: "Reimprimir relatorio", url: "#" },
     { id: 2, title: "Buscar relatorio", url: "#" },
     { id: 3, title: "Gerenciar funcionários", url: "#" },
-    { id: 3, title: "Atualizar Banco de dados", url: "#" },
-    { id: 3, title: "Carregar XML", url: "#" },
-    { id: 3, title: "Gerar arquivo do SSWsistema", url: "#" },
-    { id: 3, title: "Gerando relatório de entrega", url: "#" },
+    { id: 4, title: "Atualizar Banco de dados", url: "#" },
+    { id: 5, title: "Carregar XML", url: "#" },
+    { id: 6, title: "Gerar arquivo do SSWsistema", url: "#" },
+    { id: 7, title: "Gerando relatório de entrega", url: "#" },
 ];
 const menulist = [
     {
         id: 0,
         title: "Minha Conta",
-        submenus: [submenuslist[0], submenuslist[3]],
+        submenus: [],
     },
     {
         id: 0,
         title: "Relatorios",
-        submenus: [submenuslist[0], submenuslist[1]],
+        submenus: [submenuslist[0], submenuslist[1], submenuslist[2]],
     },
     {
         id: 1,
         title: "Banco de Dados",
-        submenus: [submenuslist[2], submenuslist[3]],
+        submenus: [submenuslist[3], submenuslist[4], submenuslist[5]],
     },
     {
         id: 1,
         title: "Ajuda",
-        submenus: [submenuslist[2], submenuslist[3]],
+        submenus: [submenuslist[6], submenuslist[7]],
     },
 ];
 
