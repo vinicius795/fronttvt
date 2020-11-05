a00fc46022c22afe17d146a0c0f3d26228ab1431
Vinicius <vinicius795@hotmail.com>
2020-11-05 20:32:27 -0300

     

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { card } from "./cards.interface"
 import { cardlist } from '../mocks/cards'
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-cards',
@@ -13,7 +16,13 @@ export class CardsComponent implements OnInit {
   _cardlist = cardlist;
   cardbase: card;
 
-  constructor() { }
+  ismobile$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
