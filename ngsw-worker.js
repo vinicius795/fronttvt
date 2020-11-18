36210474be59ac4a151c38ca328fbd3ef3efef41
vinicius795 <vinicius795@hotmail.com>
2020-11-18 02:33:07 -0300

    Update ngsw-worker.js

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1,3 +1,19 @@
+self.addEventListener('fetch', function (event) {
+    const request = event.request;
+    
+    // filter for html document fetches (should only result in one single fetch) --> index.html
+    if (request.method === "GET" && request.destination === "document") {
+
+        // only intercept if there was a problem fetching index.html
+        event.respondWith(
+            fetch(request).catch(function (error) {
+                console.error("[onfetch] Failed. Serving cached offline fallback", error);
+
+                // return offline page from cache instead
+                return caches.match("/assets/offline.html");
+            }));
+    }
+});
 (function () {
     'use strict';
 
