8ecf8c2d1160d20a12fc1137596ea24df0f8afb3
vinicius795 <vinicius795@hotmail.com>
2021-01-06 00:32:57 -0300

     

--- src/app/print-r-entregas/print-r-entregas.component.ts
+++ src/app/print-r-entregas/print-r-entregas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
+import { PrintService } from '../print.service';
 
 @Component({
   selector: 'app-print-r-entregas',
@@ -8,13 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PrintREntregasComponent implements OnInit {
   invoiceIds: string[];
+  invoiceDetails: Promise<any>[];
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute,
+    private printService: PrintService) {
     this.invoiceIds = route.snapshot.params['invoiceIds']
       .split(',');
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.invoiceDetails = this.invoiceIds
+      .map(id => this.getInvoiceDetails(id));
+    Promise.all(this.invoiceDetails)
+      .then(() => this.printService.onDataReady());
+  }
+
+  getInvoiceDetails(invoiceId) {
+    const amount = Math.floor((Math.random() * 100));
+    return new Promise(resolve =>
+      setTimeout(() => resolve({ amount }), 1000)
+    );
   }
 
 }
